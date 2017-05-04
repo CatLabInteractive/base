26501e3fb48febe4a91395fe5160e22653adaa82
daedeloth <thijs@catlab.be>
2017-05-04 10:59:12 +0200

    Add string helper.

---
+++ src/CatLab/Base/Helpers/StringHelper.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace CatLab\Base\Helpers;
+
+/**
+ * Class StringHelper
+ * @package CatLab\Base\Helpers;
+ */
+class StringHelper
+{
+    /**
+     * @param $str
+     * @param $start
+     * @param null $length
+     * @param null $encoding
+     * @return string
+     */
+    public static function substr($str, $start, $length = null, $encoding = null)
+    {
+        if ($encoding === null) {
+            $encoding = mb_internal_encoding();
+        }
+
+        return mb_substr($str, $start, $length, $encoding);
+    }
+
+    /**
+     * @param $str
+     * @param null $encoding
+     * @return int
+     */
+    public static function length($str, $encoding = null)
+    {
+        if ($encoding === null) {
+            $encoding = mb_internal_encoding();
+        }
+
+        return mb_strlen($str, $encoding);
+    }
+
+    /**
+     * @param string $input
+     * @return string
+     */
+    public static function escapeFileName($input)
+    {
+        return preg_replace('/[^A-Za-z0-9_\-]/', '_', $input);
+    }
+
+    /**
+     * @param $input
+     * @return string
+     */
+    public static function toLower($input)
+    {
+        return mb_strtolower($input);
+    }
+}
